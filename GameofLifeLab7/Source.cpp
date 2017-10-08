f7a0d200a9d259b67c9bff5fe3841b18b974eb96
Rick Storer <roderickstorer@gmail.com>
2017-10-08 17:20:13 -0500

    Board displays and generations advance automatically

--- GameofLifeLab7/Source.cpp
+++ GameofLifeLab7/Source.cpp
@@ -9,10 +9,35 @@ using namespace std;
 
 void main()
 {
-	const	int NumRows (10);
-	const	int NumCols (10);
-			bool Board [NumRows + 2] [NumCols + 2];
-			bool Next [NumRows + 2] [NumCols + 2];
+	const	int		NumRows (60);
+	const	int		NumCols (60);
+	const	time_t	WaitTime(3);
 
+			bool	Board [NumRows + 2] [NumCols + 2];
+			bool	Next [NumRows + 2] [NumCols + 2];
 
+			time_t	CurrTime;
+
+			int		Generation;
+			int		NumNeighbors;
+			int		Col;
+			int		Row;
+
+			memset (Board, false, ((NumRows + 2) * (NumCols + 2) * sizeof (bool)));
+
+			for (Generation = 0; ; Generation++)
+				{
+				cout << "\tGeneration: " << Generation << endl;
+				for (Row = 1; Row <= NumRows; Row++)
+					{
+					for (Col = 1; Col <= NumCols; Col++)
+						cout << (Board [Row] [Col] ? '*' : ' ');
+					cout << endl;
+					}
+				CurrTime = time(0);
+				while (time(0) < (CurrTime + WaitTime));
+				system("cls");
+				}
+
+	system("pause");
 }
