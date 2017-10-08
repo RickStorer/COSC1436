9da6baa0ea6c6727e849859cab19dbdca0c7a3d8
Rick Storer <roderickstorer@gmail.com>
2017-10-08 16:32:22 -0500

    Initial commit

---
+++ GameofLifeLab7/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+#include <stdlib.h>
+#include <conio.h>
+#include <memory.h>
+#include <time.h>
+
+void main()
+{
+	const	int NumRows (10);
+	const	int NumCols (10);
+			bool Board [NumRows + 2] [NumCols + 2];
+			bool Next [NumRows + 2] [NumCols + 2];
+
+
+}
